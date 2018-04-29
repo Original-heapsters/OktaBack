73f4002bf2f03d63b130d431b32d575612d6e1ac
Jeimmi <jeimmi.gomez@gmail.com>
2018-04-29 02:11:14 -0700

    a

--- app.py
+++ app.py
@@ -26,30 +26,30 @@ meta.bind = db
 
 class User(db.Model):
     __tablename__ = 'users'
-    ID = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    id = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     firstName = db.Column(db.String(10), nullable=False)
     lastName = db.Column(db.String(10), nullable=False)
     radiusSettings = db.Column(db.Integer)
 
     def serialized(self):
-        return { 'ID':self.ID,
+        return { 'id':self.id,
         'firstName':self.firstName,
         'lastName':self.lastName,
         'radiusSettings':self.radiusSettings
         }
     def __repr__(self):
-        return "<User(firstName='%s', lastName='%s', ID='%s')>" % (self.firstName, self.lastName, self.ID)
+        return "<User(firstName='%s', lastName='%s', id='%s')>" % (self.firstName, self.lastName, self.id)
 
 class Asset(db.Model):
     __tablename__ = 'assets'
-    ID = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-    owner = db.Column(db.String(80), db.ForeignKey('users.ID'), nullable=False)
+    id = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    owner = db.Column(db.String(80), db.ForeignKey('users.id'), nullable=False)
     link = db.Column(db.String(256))
-    type = db.Column(Enum("image", "3d", "video"))
+    type = db.Column(Enum("image", "3d", "video","dae", "mp4", "png", "jpg", "jpeg", "mp3", "mov"))
 
     def serialized(self):
         return {
-        'ID':self.ID,
+        'id':self.id,
         'owmer':self.owner,
         'link':self.link,
         'type':self.type
@@ -58,22 +58,37 @@ class Asset(db.Model):
 
 class Mark(db.Model):
     __tablename__ = 'marks'
-    idMark = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-    user: db.Column(db.String(80), db.ForeignKey('users.ID'), nullable=False)
-    note: Column(String(200))
+    id = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    user= db.Column(db.String(80), db.ForeignKey('users.id'), nullable=False)
+    #user= db.Column(db.String(80), nullable=False)
+    note= Column(String(200))
 
     def serialized(self):
         return {
-        'idMark':self.idMark,
+        'id':self.id,
         'user':self.user,
         'note':self.note
         }
 
-DefaultUser = { 'id':'1asf2sg3gdfg456g7f890', 'username':'xXChuckersXx420', 'firstName' : 'Chuck', 'lastName' : 'Beans', 'radiusSettings':20, 'placedList' : ['7ghf87gfgw7fg87g', 'frgd545y4g4gr','wegf34t45grthe4ewg','ew4gerethrh5rhh','wefgwe4egegsrgerg'], 'markedList':['sdfsgdbb54rsr6s5hbh45','b45w56nb5n6e5n','h5w6srnb56n5n','6he56hn5yjnd5j','56je5heserhs5rh5','e5hsrhrssrjs6jsr'] }
+# DefaultUser = { 'id':'1asf2sg3gdfg456g7f890', 'username':'xXChuckersXx420', 'firstName' : 'Chuck', 'lastName' : 'Beans', 'radiusSettings':20, 'placedList' : ['7ghf87gfgw7fg87g', 'frgd545y4g4gr','wegf34t45grthe4ewg','ew4gerethrh5rhh','wefgwe4egegsrgerg'], 'markedList':['sdfsgdbb54rsr6s5hbh45','b45w56nb5n6e5n','h5w6srnb56n5n','6he56hn5yjnd5j','56je5heserhs5rh5','e5hsrhrssrjs6jsr'] }
 
-DefaultAsset = {'id':'sdkfuh78hfih8', 'owner':'1asf2sg3gdfg456g7f890', 'link':'iwh87a4gh8w7gh8g.dae', 'markedBy':['sfgzsrgs54gser5h','5sehsrhrh','se5hsrhrthsr5h','w43gw3aw3awag','hr6jtd7kfy7kfkj','gergsregzw4g']}
+# DefaultAsset = {'id':'sdkfuh78hfih8', 'owner':'1asf2sg3gdfg456g7f890', 'link':'iwh87a4gh8w7gh8g.dae', 'markedBy':['sfgzsrgs54gser5h','5sehsrhrh','se5hsrhrthsr5h','w43gw3aw3awag','hr6jtd7kfy7kfkj','gergsregzw4g']}
 
-DefaultMark = {'id':'d9a8dhfhsiufhis','userId':'1asf2sg3gdfg456g7f890','note':'xXCHUCKIEXx BBBOOIIIIIIIIII'}
+# DefaultMark = {'id':'d9a8dhfhsiufhis','userId':'1asf2sg3gdfg456g7f890','note':'xXCHUCKIEXx BBBOOIIIIIIIIII'}
+
+# db.create_all()
+
+# newUser = User(id='jeimmi123', firstName='jeimmi',lastName='gomez', radiusSettings=20)
+# db.session.add(newUser)
+# db.session.commit()
+
+# newAsset = Asset(id='assetIDtest', owner=newUser.id, link='iwh87a4gh8w7gh8g.dae', type='dae')
+# db.session.add(newAsset)
+# db.session.commit()
+
+# newMark = Mark(id='d9a8dhfhsiufhis', user='1asf2sg3gdfg456g7f890', note="WORK")
+# db.session.add(newMark)
+# db.session.commit()
 
 @app.route('/')
 def index():
@@ -130,23 +145,24 @@ def user():
         print(username)
         print(radiusSettings)
         userObj = createUser(userId,radiusSettings,firstName,lastName)
-        print (userObj)
         response['status'] = 200
         response['message'] = 'User successfully created'
         response['data'] = userObj.serialized()
 
 
     elif request.method == 'GET':
-       username = request.args.get('username', '')
-       if username is None:
+       userId = request.args.get('userId', '')
+       if userId is None:
            response['status'] = 404
-           response['message'] = 'username was missing'
+           response['message'] = 'userId was missing'
            retResp = jsonify(response)
            return retResp
-       data = DefaultUser#DBMan.GetUser(username)
+
+       data = getUser(userId)
+
        response['status'] = 200
        response['message'] = 'User successfully fetched'
-       response['data'] = data
+       response['data'] = data.serialized()
 
     else:
        response['status'] = 404
@@ -165,13 +181,15 @@ def place():
         return retResp
 
     userId = request.args.get('userId', None)
+    lat = request.args.get('lat', None)
+    lon = request.args.get('lon', None)
+    assetType = request.args.get('assetType', None)
+
     if userId is None:
         response['status'] = 404
         response['message'] = 'userId missing'
         retResp = jsonify(response)
         return retResp
-    lat = request.args.get('lat', None)
-    lon = request.args.get('lon', None)
 
     if lat is None or lon is None:
         response['status'] = 404
@@ -180,7 +198,7 @@ def place():
         return retResp
 
     latLongString = str(lat) + ',' + str(lon)
-    assetType = request.args.get('assetType', None)
+    
     if assetType is None:
         response['status'] = 404
         response['message'] = 'Asset type is missing'
@@ -208,7 +226,7 @@ def place():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     asset = filename
     assetLink = request.url_root + 'static/' + asset
-    #data = DBMan.placeAsset(assetId,userId,assetLink,assetType,latLongString)
+    data = placeAsset(assetId,userId,assetLink,assetType,latLongString)
     data = {}
     data['assetId'] = assetId
     data['owner'] = userId
@@ -218,7 +236,7 @@ def place():
 
     response['status'] = 200
     response['message'] = 'Successfully posted asset'
-    response['data'] = data
+    response['data'] = data.serialized()
     retResp = jsonify(response)
     return retResp
 
@@ -238,12 +256,19 @@ def nearby(radius=None, locationString=None):
 @app.route('/found/<assetId>', methods=['GET'])
 def found(assetId=None):
     response = {}
-    data = DefaultAsset #DBMan.found(assetId)
+    if request.method != 'GET':
+        response['status'] = 404
+        response['message'] = '/found only supports POST requests'
+        retResp = jsonify(response)
+        return retResp 
+
+    data =found(assetId)
     response['status'] = 200
     response['message'] = 'Successfully fetched found asset'
-    response['data'] = data
-    retResp = jsonify(response)
-    return retResp
+    if data is not None:
+        response['data'] = data.serialized()
+        retResp = jsonify(response)
+        return retResp
 
 @app.route('/mark/<assetId>', methods=['POST'])
 def mark(assetId=None):
@@ -279,8 +304,8 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def createUser(id, radiusSettings, first=None, last = None,):
-    exists = User.query.filter_by(ID=id).first()
-    newUser = User(ID=id, radiusSettings=radiusSettings, firstName=first, lastName=last)
+    exists = User.query.filter_by(id=id).first()
+    newUser = User(id=id, radiusSettings=radiusSettings, firstName=first, lastName=last)
     if not exists:
         db.session.add(newUser)
         db.session.commit()
@@ -288,17 +313,34 @@ def createUser(id, radiusSettings, first=None, last = None,):
     else:
         return newUser
 
-def get_or_create(session, model, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).first()
+def getUser(id):
+    instance = db.session.query(User).filter_by(id=id).first()
+    if instance:
+        return instance
+#    else:
+        #instance = createUser(id, app.config['DEFAULT_RADIUS'])
+        #session.add(instance)
+        #session.commit()
+#        return jsonify("User not found")     
+
+def placeAsset(assetId,userId,link,type,latLongString=None):
+    exists = Asset.query.filter_by(id=assetId).first()
+    newAsset = Asset(id=assetId, owner=userId, link=link, type=type)
+    print(newAsset.owner)
+    if not exists:
+        db.session.add(newAsset)
+        db.session.commit()
+        print(newAsset.owner)
+        return newAsset
+    else:
+        return newAsset
+
+def found(id):
+    instance = db.session.query(Asset).filter_by(id=id).first()
     if instance:
         return instance
     else:
-        instance = model(**kwargs)
-        session.add(instance)
-        session.commit()
-        return instance
-
-
+        return instance      
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
